3eb12892bc44b9ba837a6a1dd22f6bc89e15ed18
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2021-04-07 13:07:15 +0530

    Added JWT helpers

---
+++ src/helper/jwt.js
@@ -0,0 +1,52 @@
+require('dotenv/config')
+const JWT = require('jsonwebtoken')
+
+const models = require('../models')
+
+const refreshToken = models.RefreshToken
+const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET,
+  REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET
+
+// Function to generate access token
+const generateAccessToken = (user) => new Promise((resolve, reject) => {
+  if (!user) reject('Please provide payload')
+  resolve(JWT.sign(user, ACCESS_SECRET, { expiresIn: '60m' }))
+})
+
+// Function to generate refresh token
+const generateRefreshToken = (user) => new Promise(async (resolve, reject) => {
+  if (!user) reject('Please provide payload')
+  const generatedRefreshToken = JWT.sign(user, REFRESH_SECRET)
+  await refreshToken.create({
+    user: user,
+    token: generatedRefreshToken
+  })
+  resolve(generatedRefreshToken)
+})
+
+// Function to verify refresh Token
+const verifyRefreshToken = (token) => {
+  return new Promise((resolve, reject) => {
+    if (!token) reject('Please provide payload')
+    JWT.verify(token, REFRESH_SECRET, async (err, user) => {
+      if (err) reject(err)
+      const refreshToken = await refreshToken.findOne({
+        token
+      })
+      if (!refreshToken) reject('Invalid Refresh Token')
+      const accessToken = await generateAccessToken({
+        username: user.username,
+        name: user.name
+      })
+      resolve({
+        accessToken: accessToken
+      })
+    })
+  })
+}
+
+module.exports = {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyRefreshToken
+}
