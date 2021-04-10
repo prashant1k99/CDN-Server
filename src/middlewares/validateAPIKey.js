ec8a95f9f114ae55279e3d112ed0a9c86bf1ebae
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2021-04-10 10:02:45 +0530

    Added the User in the API middleware

--- src/middlewares/validateAPIKey.js
+++ src/middlewares/validateAPIKey.js
@@ -9,9 +9,12 @@ const validateAPIkey = async (req, res, next) => {
     if (!userKey) return res.status(401).send('No API key provided')
     const fetchedKey = await API.findOne({
       apiKey: userKey
-    })
+    }).populate('user')
     const domainsWhitelistedForKey = fetchedKey._doc.whiteListedDomain
-    if (domainsWhitelistedForKey.includes(originURL)) next()
+    if (domainsWhitelistedForKey.includes(originURL)) {
+      req.user = fetchedKey._doc.user
+      next()
+    }
     res.status('401').send('Request domain is not allowed for the API key.')
   } catch (err) {
     res.status(500).send(err)
